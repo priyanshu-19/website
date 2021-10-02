319d1c419713bf5295e730001f758cfd9013425e
priyanshu-19 <79503533+priyanshu-19@users.noreply.github.com>
2021-10-02 20:46:33 +0530

    Add files via upload

---
+++ SpurtGo Website/searchindex.js
@@ -0,0 +1,18 @@
+// search index for WYSIWYG Web Builder
+var database_length = 0;
+
+function SearchPage(url, title, keywords, description)
+{
+   this.url = url;
+   this.title = title;
+   this.keywords = keywords;
+   this.description = description;
+   return this;
+}
+
+function SearchDatabase()
+{
+   database_length = 0;
+   this[database_length++] = new SearchPage("index.html", "Untitled Page", "Send your message in the form below and we will get back to you as soon as possible   ", "");
+   return this;
+}
